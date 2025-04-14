6f25b27401c47f7f5aae83cbeae7f751b08a6fdd
anderRaya <anderraya10@gmail.com>
2025-04-14 09:22:38 +0200

    Update Peoia.java
    mugimendu egokia peoia

--- xakeaProiektua/src/org/pmobo/xakea/Peoia.java
+++ xakeaProiektua/src/org/pmobo/xakea/Peoia.java
@@ -7,9 +7,57 @@ public class Peoia extends Pieza {
 
 	//eraikitzailea
 	public Peoia(boolean pZuriaDa) {
-		super(pZuriaDa, false);
+		super(pZuriaDa, false, pLaukia);
 		this.mugituDa = false;
 	}
-	
+	@Overraid
+	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
+		if(this.mugituDA==false){
+			if(super.zuriaDA==true){
+				if((pY-pLaukia.getY()==1 || pY-pLaukia.getY()==2) && pX==pLaukia.getX()){
+					if(okupatutaDago(pX, pY)==false){
+						return true;
+					} else{return false;}
+				} else if(pY-pLaukia.getY()==1 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)){
+					if(okupatutaDago(pX, pY)==true){
+						return true;
+					} else{return false;}
+				}
+			} else{
+				if((pY-pLaukia.getY()==-1 || pY-pLaukia.getY()==-2) && pX==pLaukia.getX()){
+					if(okupatutaDago(pX, pY)==false){
+						return true;
+					} else{return false;}
+				} else if(pY-pLaukia.getY()==-1 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)){
+					if(okupatutaDago(pX, pY)==true){
+						return true;
+					} else{return false;}
+				}
+			}
+		} else{
+			if(super.zuriaDA==true){
+				if(pY-pLaukia.getY()==1 && pX==pLaukia.getX()){
+					if(okupatutaDago(pX, pY)==false){
+						return true;
+					} else{return false;}
+				} else if(pY-pLaukia.getY()==1 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)){
+					if(okupatutaDago(pX, pY)==true){
+						return true;
+					} else{return false;}
+				}
+			} else{
+				if(pY-pLaukia.getY()==-1 && pX==pLaukia.getX()){
+					if(okupatutaDago(pX, pY)==false){
+						return true;
+					} else{return false;}
+				} else if(pY-pLaukia.getY()==-1 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)){
+					if(okupatutaDago(pX, pY)==true){
+						return true;
+					} else{return false;}
+				}
+			}
+			
+		}
+	}
 	
 }
