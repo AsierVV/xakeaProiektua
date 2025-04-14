9c748b71318bc125cf514da0787a4a9bd8e0c2a8
anderRaya <anderraya10@gmail.com>
2025-04-14 10:05:28 +0200

    Update Dorrea.java
    mugimendu egokia Dorrea

--- xakeaProiektua/src/org/pmobo/xakea/Dorrea.java
+++ xakeaProiektua/src/org/pmobo/xakea/Dorrea.java
@@ -6,4 +6,19 @@ public class Dorrea extends Pieza{
 	public Dorrea(boolean pZuriaDa, boolean pGalduta, Laukia pKoordenatuak) {
 		super(pZuriaDa, pGalduta, pKoordenatuak);
 	}
+
+	@Override
+	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
+		if ((pX==pLuakia.getX() && pY!=pLaukia.getY()) || ((pX!=pLuakia.getX() && pY==pLaukia.getY()))) {
+			if(bideanZerbait(pX, pY, pLaukia.getX(). pLaukia.getY())){
+				return false
+			} else {
+				//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
+				//              --> true: okupatuta dago bere kolore berdina duen piezaz
+				if (okupatutaDago(pX, pY, pZuriaDa)){
+					return false;
+				} else {return true;}
+			}
+		} else{return false;}
+	}
 }
