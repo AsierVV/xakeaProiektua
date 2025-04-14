55c68865fba386b279111196c520d16823e18b8c
anderRaya <anderraya10@gmail.com>
2025-04-14 09:47:03 +0200

    Update Erregea.java
    mugimendu egpokia rey

--- xakeaProiektua/src/org/pmobo/xakea/Erregea.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregea.java
@@ -6,4 +6,15 @@ public class Erregea extends Pieza{
 	public Erregea(boolean pZuriaDa, boolean pGalduta, Laukia pKoordenatuak) {
 		super(pZuriaDa, pGalduta, pKoordenatuak);
 	}
+
+	@Override
+	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
+		if((pX==pLuakia.getX() && pY-pLaukia.get()==1) || (pX-pLuakia.getX()==1 && pY-pLaukia.get()==1) || (pX-pLuakia.getX()==1 && pY==pLaukia.get()) || (pX-pLuakia.getX()==1 && pY-pLaukia.get()==-1) || (pX==pLuakia.getX() && pY-pLaukia.get()==-1) || (pX-pLuakia.getX()==-1 && pY-pLaukia.get()==-1) || (pX-pLuakia.getX()==-1 && pY==pLaukia.get()) || (pX-pLuakia.getX()==-1 && pY-pLaukia.get()==1)){
+			//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
+			//              --> true: okupatuta dago bere kolore berdina duen piezaz
+			if(okupatutaDago(pX, pY, pZuriaDa)){
+				return false;
+			} else {return true;}
+		} else {return false;}
+	}
 }
