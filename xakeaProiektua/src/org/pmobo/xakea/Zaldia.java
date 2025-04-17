5c8a9fd4fa1d29a57d8f95338f4f55abdafd932e
anderRaya <anderraya10@gmail.com>
2025-04-17 20:04:04 +0200

    Update Zaldia.java
    mugimenduEgokia

--- xakeaProiektua/src/org/pmobo/xakea/Zaldia.java
+++ xakeaProiektua/src/org/pmobo/xakea/Zaldia.java
@@ -6,5 +6,19 @@ public class Zaldia extends Pieza {
 	public Zaldia(boolean pZuriaDa, boolean pGalduta, Laukia pKoordenatuak) {
 		super(pZuriaDa, pGalduta, pKoordenatuak);
 	}
-	
+	@Override
+	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia) {
+		if((pY-pLaukia.getY()==2 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)) || (pY-pLaukia.getY()==-2 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)) || (pX-pLaukia.getX()==2 && (pY-pLaukia.getY()==1 || pY-pLaukia.getY()==-1)) || (pX-pLaukia.getX()==-2 &&(pY-pLaukia.getY()==1 || pY-pLaukia.getY()==-1))) {
+			if(bideanZerbait(pX, pY, pLaukia.getX(). pLaukia.getY())) {
+				return false;
+			} else {
+				//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
+				//              --> true: okupatuta dago bere kolore berdina duen piezaz
+				if (okupatutaDago(pX, pY, pZuriaDa)){
+					return false;
+				} else {return true;}
+			}
+		} else{return false;}
+	}
+			
 }
