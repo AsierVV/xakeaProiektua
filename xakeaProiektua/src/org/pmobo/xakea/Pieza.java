4a2ee784d99094ab73b823d146ca236ac7de4c5f
AsierVV <avaldelviravazquez@gmail.com>
2025-04-19 20:06:21 +0200

    Update Pieza.java
    Añadido el metodo "piezaJan"
    
    Cambios en el CONSTRUCTOR. Solo le entrar dos valores, zuriaDA y laukia. Ya que cuando se inicia el valor de galduta siempre va a ser false.
    
    pKoordenatuak --> pLaukia
    Mejor llamarlo pLaukia ya que es un objeto tipo LAUKIA

--- xakeaProiektua/src/org/pmobo/xakea/Pieza.java
+++ xakeaProiektua/src/org/pmobo/xakea/Pieza.java
@@ -5,28 +5,34 @@ public abstract class Pieza {
 	//atributuak
 	private boolean zuriaDa;
 	private boolean galduta;
-	private Laukia koordenatuak;
+	private Laukia laukia;
 	
 	//eraikitzailea
-	public Pieza(boolean pZuriaDa, boolean pGalduta, Laukia pKoordenatuak) {
+	public Pieza(boolean pZuriaDa, Laukia pLaukia) {
 		this.zuriaDa = pZuriaDa;
-		this.galduta = pGalduta;
-		this.koordenatuak = pKoordenatuak;
+		this.galduta = false;
+		this.koordenatuak = pLaukia;
 	}
+	
 	public abstract boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia);
+	
 	public abstract String toString();
 
 	public boolean getZuriaDa(){
 		return this.zuriaDa;
 	}
+	
 	public boolean galduDa() {
 		return this.galduta;
 	}
+	
 	public void setGalduta(boolean galduta) {
         this.galduta = galduta;
-    }
-	public void piezaJan(Pieza p){
-		SIN HACER
+	}
+	
+	public void piezaJan(){
+		this.laukia.setKoordenatuak(-1,-1);
+		this.galduta=true;
 	}
 	
 	
