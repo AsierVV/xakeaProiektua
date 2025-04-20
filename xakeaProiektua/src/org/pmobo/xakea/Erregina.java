c0a4fbc8de237576b0c387ff51d8f42ebc29d960
anderRaya <anderraya10@gmail.com>
2025-04-20 18:50:03 +0200

    Update Erregina.java
    He heco el mugimenduEgokia. He juntado los mugimenduEgokias de dorrea y alfila, porque son sus movimientos posibles

--- xakeaProiektua/src/org/pmobo/xakea/Erregina.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregina.java
@@ -9,6 +9,16 @@ public class Erregina extends Pieza{
 
 	@Override
 	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
-
-  }
+		if((pX==pLaukia.getX() && pY!=pLaukia.getY()) || ((pX!=pLaukia.getX() && pY==pLaukia.getY()) || Math.abs(pX-pLaukia.getX())==Math.abs(pY-pLaukia.getY()))) {
+			if(bideanZerbait(pX, pY, pLaukia.getX(). pLaukia.getY())){
+				return false;
+			} else {
+				//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
+				//              --> true: okupatuta dago bere kolore berdina duen piezaz
+				if (okupatutaDago(pX, pY, pZuriaDa)){
+					return false;
+				} else {return true;}
+			}
+		} else{return false;}
+	}
 }
