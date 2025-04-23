090af56877ee2c157319868d3c2518495dd45b5f
AsierVV <avaldelviravazquez@gmail.com>
2025-04-23 16:10:34 +0200

    Update PiezaZerrenda.java
    He hecho el metodo bideanZerbait, lo he explicado todo con comentarios para que se entienda que he usado comandos que no sabiamos que existian.

--- xakeaProiektua/src/org/pmobo/xakea/PiezaZerrenda.java
+++ xakeaProiektua/src/org/pmobo/xakea/PiezaZerrenda.java
@@ -29,19 +29,43 @@ public class PiezaZerrenda {
 
 	//Comprueba que en el camino que hace la pieza para moverse no haya ninguna pieza, da igual el color. No comprueba la casilla final
 	public boolean bideanZerbait(int pX, int pY, int pX0, int pY0){
-		
+		int dx = Integer.compare(pX, pX0); // X ardatzaren norabide mugimendua: -1 -> mugitzen da ezkerrera || 0 -> ez da X ardatzan mugitzen || 1 -> mugitzen da eskuinera
+    		int dy = Integer.compare(pY, pY0); // Y ardatzaren norabide mugimendua: -1 -> mugitzen da ezkerrera || 0 -> ez da Y ardatzan mugitzen || 1 -> mugitzen da eskuinera
+
+		// x eta y aldagaiak erabiliko dira piezak egiten duen bidea zeharkatzeko. Hasieratuko dira pieza mugitzen den lehenengo laukian. dx eta dy erabiltzen da, horrela adibidez
+		// x ardatzean eskuinera mugitzen bada, x aldagaia +1 egiten joango da, eta y ardatzean ez bada mugitzen, +0 egingo da. Hau bakarrik funtzionatzen du mugimendu bertikalean,
+		// horizontalean eta diagonalean; baina zaldiak piezak saltatzen dituzte, beraz ez da behar gehiago.
+		int x = pX0 + dx;
+    		int y = pY0 + dy;
+
+		while (x != pX || y != pY) {
+        		for (Pieza p : this.lista) { // Comando honekin lortzen dugu lista oso bat zeharkatzea banan banan, kasu honetan PiezaZerrenda lista, beraz zerrendaren pieza guztiak hartuko dira banan banan, pieza bat hartuz buelta bakoitzean.
+            			if (!p.getGalduta()) { // Konprobatuko dugu hartu dugun pieza janda (galduta) dagoen ala ez, galduta badago, ez dago tableroan.
+                			Laukia pos = p.getKoordenatuak(); // Ez badago galduta, piezaren korrdenatuak gordetzen ditugu "Laukia pos" aldagaian.
+                			if (pos.getX() == x && pos.getY() == y) { // Konprobatzen dugu ea koordenatu horiek gure bidearen koordenatu berdinak diren ala ez.
+                    				return true; // Koordenatuak berdinak badira, bidean zerbait dago, beraz bueltan true. Bestela, hurrengo pieza konprobatzen dugu "for"-aren hurrengo bueltan.
+                			}
+            			}
+        		}
+			// Bidearen lehenengo laukiarekin amaitzean, hurrengo laukira pauso bat ematen dugu. Eta ez bada bidearen azkenengo laukia berriro prozesu guztia errepikatuko da.
+        		x += dx;
+        		y += dy;
+    		}
+    		return false; // Ez badugu ezer aurkitu bide osoan, bidea libre dago. Buelta false.
 	}
+
+	
 	public boolean okupatutaDago(int x, int y, boolean pKolorea) {
 		 Pieza pieza = Taula.getTaula().getPieza(x, y);
 
-		 // Si no hay pieza, entonces no está ocupada 
+		 // Piezarik ez badago, ez dago okupatuta.
 		 if (pieza == null) {
-		        return false;
+		        return false; // false bueltatzen da.
 		 }
-		 // Si hay una pieza del mismo color no se puede mover allí
+		 // Kolore berdineko piezarik badago, okupatuta dago.
 		 if (pieza.getZuriaDa() == pKolorea) {
-		        return true;
-		} else {return false;}
+		        return true; // true bueltatzen da, kolore berdineko pieza dago.
+		} else {return false;} // false bueltatzen da, kolore ezberdineko pieza dago. Beraz, "mugitu" metodoa egiterakoan, leku horretan dagoen pieza jango (galduko) da.
 
 	 }
 }
