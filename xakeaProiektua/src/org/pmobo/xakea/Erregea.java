deeed3ecd7bd82194a50ec9bb78a63c2b7011ef9
anderRaya <anderraya10@gmail.com>
2025-04-24 18:14:05 +0200

    Update Erregea.java
    He hecho el metodo toString que es para que aparezca el simbolo de la pieza en el tablero. Tambien he quitado del super eGalduta, porque eso no tenemos definido. Tenemos puesto false, pero al poner false tambien da error asique creo que no hay que poner nada en ese caso.

--- xakeaProiektua/src/org/pmobo/xakea/Erregea.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregea.java
@@ -4,17 +4,21 @@ public class Erregea extends Pieza{
 
 	//eraikitzailea
 	public Erregea(boolean pZuriaDa, Laukia pLaukia) {
-		super(pZuriaDa, pGalduta, pLaukia);
+		super(pZuriaDa, pLaukia);
 	}
 
 	@Override
 	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
-		if((pX==pLaukia.getX() && pY-pLaukia.get()==1) || (pX-pLaukia.getX()==1 && pY-pLaukia.get()==1) || (pX-pLaukia.getX()==1 && pY==pLaukia.get()) || (pX-pLaukia.getX()==1 && pY-pLaukia.get()==-1) || (pX==pLaukia.getX() && pY-pLaukia.get()==-1) || (pX-pLaukia.getX()==-1 && pY-pLaukia.get()==-1) || (pX-pLaukia.getX()==-1 && pY==pLaukia.get()) || (pX-pLaukia.getX()==-1 && pY-pLaukia.get()==1)){
+		if((pX==pLaukia.getX() && pY-pLaukia.getY()==1) || (pX-pLaukia.getX()==1 && pY-pLaukia.getY()==1) || (pX-pLaukia.getX()==1 && pY==pLaukia.getY()) || (pX-pLaukia.getX()==1 && pY-pLaukia.getY()==-1) || (pX==pLaukia.getX() && pY-pLaukia.getY()==-1) || (pX-pLaukia.getX()==-1 && pY-pLaukia.getY()==-1) || (pX-pLaukia.getX()==-1 && pY==pLaukia.getY()) || (pX-pLaukia.getX()==-1 && pY-pLaukia.getY()==1)){
 			//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
 			//              --> true: okupatuta dago bere kolore berdina duen piezaz
 			if(okupatutaDago(pX, pY, pZuriaDa)){
 				return false;
 			} else {return true;}
 		} else {return false;}
+	}
+	@Override
+	public String toString() {
+	    return getZuriaDa() ? "♔" : "♚";  // Rey blanco y negro
 	}
 }
