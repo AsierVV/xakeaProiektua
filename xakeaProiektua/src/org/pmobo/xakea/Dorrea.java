7d9c4d5d1f1b0b9d4c8b0018db8805191aaba36c
AsierVV <avaldelviravazquez@gmail.com>
2025-04-25 22:21:20 +0200

    Update Dorrea.java
    Corregido teniendo en cuenta los errores de eclipse

--- xakeaProiektua/src/org/pmobo/xakea/Dorrea.java
+++ xakeaProiektua/src/org/pmobo/xakea/Dorrea.java
@@ -18,7 +18,7 @@ public class Dorrea extends Pieza{
 			} else {
 				//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
 				//              --> true: okupatuta dago bere kolore berdina duen piezaz
-				if (okupatutaDago(pX, pY, pKolorea)){
+				if(Taula.getTaula().okupatutaDago(pX, pY, this.getZuriaDa())){
 					return false;
 				} else {return true;}
 			}
