e66e09660a416f74a5be60ab0ad4f441f5f7c86c
marvoloelmago <marcomateosgallego@gmail.com>
2025-04-25 13:44:46 +0200

    Update Erregina.java

--- xakeaProiektua/src/org/pmobo/xakea/Erregina.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregina.java
@@ -3,17 +3,8 @@ package org.pmobo.xakea;
 public class Erregina extends Pieza{
 
 	//eraikitzailea
-	public Erregina(boolean pZuriaDa, Laukia pLaukia) {
-		super(pZuriaDa, pLaukia);
-	}
-
-	@Override
-	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
-		public class Erregina extends Pieza{
-
-	//eraikitzailea
-	public Erregina(boolean pZuriaDa, Laukia pLaukia) {
-		super(pZuriaDa, pLaukia);
+	public Erregina(boolean pZuriaDa) {
+		super(pZuriaDa);
 	}
 
 	@Override
@@ -21,23 +12,6 @@ public class Erregina extends Pieza{
 		if (pX < 0 || pX > 7 || pY < 0 || pY > 7) {
 	        return false;
 		}
-		if((pX==pLaukia.getX() && pY!=pLaukia.getY()) || ((pX!=pLaukia.getX() && pY==pLaukia.getY()) || Math.abs(pX-pLaukia.getX())==Math.abs(pY-pLaukia.getY()))) {
-			if(bideanZerbait(pX, pY, pLaukia.getX(), pLaukia.getY())){
-				return false;
-			} else {
-				//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
-				//              --> true: okupatuta dago bere kolore berdina duen piezaz
-				if (okupatutaDago(pX, pY, pZuriaDa)){
-					return false;
-				} else {return true;}
-			}
-		} else{return false;}
-	}
-	@Override
-	public String toString() {
-	    return getZuriaDa() ? "♕" : "♛";  
-	}
-}
 		if((pX==pLaukia.getX() && pY!=pLaukia.getY()) || ((pX!=pLaukia.getX() && pY==pLaukia.getY()) || Math.abs(pX-pLaukia.getX())==Math.abs(pY-pLaukia.getY()))) {
 			if(bideanZerbait(pX, pY, pLaukia.getX(), pLaukia.getY())){
 				return false;
