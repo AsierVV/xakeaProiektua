11a9227df12fe624ae9c1f0753139470e9618704
anderRaya <anderraya10@gmail.com>
2025-04-25 13:16:29 +0200

    Update Erregina.java
    He añadido un ¨If¨ para que la pieza no pueda salirse del tablero

--- xakeaProiektua/src/org/pmobo/xakea/Erregina.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregina.java
@@ -9,6 +9,35 @@ public class Erregina extends Pieza{
 
 	@Override
 	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
+		public class Erregina extends Pieza{
+
+	//eraikitzailea
+	public Erregina(boolean pZuriaDa, Laukia pLaukia) {
+		super(pZuriaDa, pLaukia);
+	}
+
+	@Override
+	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
+		if (pX < 0 || pX > 7 || pY < 0 || pY > 7) {
+	        return false;
+		}
+		if((pX==pLaukia.getX() && pY!=pLaukia.getY()) || ((pX!=pLaukia.getX() && pY==pLaukia.getY()) || Math.abs(pX-pLaukia.getX())==Math.abs(pY-pLaukia.getY()))) {
+			if(bideanZerbait(pX, pY, pLaukia.getX(), pLaukia.getY())){
+				return false;
+			} else {
+				//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
+				//              --> true: okupatuta dago bere kolore berdina duen piezaz
+				if (okupatutaDago(pX, pY, pZuriaDa)){
+					return false;
+				} else {return true;}
+			}
+		} else{return false;}
+	}
+	@Override
+	public String toString() {
+	    return getZuriaDa() ? "♕" : "♛";  
+	}
+}
 		if((pX==pLaukia.getX() && pY!=pLaukia.getY()) || ((pX!=pLaukia.getX() && pY==pLaukia.getY()) || Math.abs(pX-pLaukia.getX())==Math.abs(pY-pLaukia.getY()))) {
 			if(bideanZerbait(pX, pY, pLaukia.getX(), pLaukia.getY())){
 				return false;
