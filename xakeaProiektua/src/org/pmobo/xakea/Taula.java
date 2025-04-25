7224a5214f73c3b56f890a36b7e67edf91885de0
AsierVV <avaldelviravazquez@gmail.com>
2025-04-25 22:02:41 +0200

    Update Taula.java
    Taula corregido con los errores de eclipse

--- xakeaProiektua/src/org/pmobo/xakea/Taula.java
+++ xakeaProiektua/src/org/pmobo/xakea/Taula.java
@@ -9,8 +9,8 @@ public class Taula {
 	//atributua
 	private Laukia[][] taula;
 	private static Taula nireTaula=null;
-	private List<Pieza> piezaZuriGaldutak;
-    	private List<Pieza> piezaBeltzGaldutak;
+	private ArrayList<Pieza> piezaZuriGaldutak;
+    	private ArrayList<Pieza> piezaBeltzGaldutak;
 	Teklatua t = Teklatua.getTeklatua(); // Hartzen dugu Teklatuaren instantzia t aldagaia Teklatua bezala erabiltzeko.
 	
 	//Singleton
@@ -43,105 +43,87 @@ public class Taula {
 			laukia = taula[x][1];
 			Peoia p = new Peoia(true);
 			taula[x][1].setPieza(p);
-			PiezaZerrenda.getEMA().gehituPieza(p);
 		}
 		//Peoi beltzak
 		for(int x=0; x<8; x++) {
 			laukia = taula[x][6];
 			Peoia p = new Peoia(false);
 			taula[x][6].setPieza(p);
-			PiezaZerrenda.getEMA().gehituPieza(p);
 		}
 		
 		//Dorre zuriak
 		laukia = taula[0][0];
 		Dorrea d1 = new Dorrea(true);
 		taula[0][0].setPieza(d1);
-		PiezaZerrenda.getEMA().gehituPieza(d1);		
 
 		laukia = taula[7][0];
 		Dorrea d2 = new Dorrea(true);
 		taula[7][0].setPieza(d2);
-		PiezaZerrenda.getEMA().gehituPieza(d2);
 		
 		//Dorre beltzak
 		laukia = taula[0][7];
 		Dorrea d3 = new Dorrea(false);
 		taula[0][7].setPieza(d3);
-		PiezaZerrenda.getEMA().gehituPieza(d3);		
 
 		laukia = taula[7][7];
 		Dorrea d4 = new Dorrea(false);
 		taula[7][7].setPieza(d4);
-		PiezaZerrenda.getEMA().gehituPieza(d4);
 		
 		//Zaldi zuriak
 		laukia = taula[1][0];
 		Zaldia z1 = new Zaldia(true);
 		taula[1][0].setPieza(z1);
-		PiezaZerrenda.getEMA().gehituPieza(z1);
 
 		laukia = taula[6][0];
 		Zaldia z2 = new Zaldia(true);
 		taula[6][0].setPieza(z2);
-		PiezaZerrenda.getEMA().gehituPieza(z2);
 		
 		//Zaldi beltzak
 		laukia = taula[1][7];
 		Zaldia z3 = new Zaldia(false);
 		taula[1][7].setPieza(z3);
-		PiezaZerrenda.getEMA().gehituPieza(z3);
 
 		laukia = taula[6][7];
 		Zaldia z4 = new Zaldia(false);
 		taula[6][7].setPieza(z4);
-		PiezaZerrenda.getEMA().gehituPieza(z4);
 		
 		//Alfil zuriak
 		laukia = taula[2][0];
 		Alfila a1 = new Alfila(true);
 		taula[2][0].setPieza(a1);
-		PiezaZerrenda.getEMA().gehituPieza(a1);
 
 		laukia = taula[5][0];
 		Alfila a2 = new Alfila(true);
 		taula[5][0].setPieza(a2);
-		PiezaZerrenda.getEMA().gehituPieza(a2);
 		
 		//Alfil beltzak
 		laukia = taula[2][7];
 		Alfila a3 = new Alfila(false);
 		taula[2][7].setPieza(a3);
-		PiezaZerrenda.getEMA().gehituPieza(a3);
 
 		laukia = taula[5][7];
 		Alfila a4 = new Alfila(false);
 		taula[5][7].setPieza(a4);
-		PiezaZerrenda.getEMA().gehituPieza(a4);
 		
 		//Errege zuria
 		laukia = taula[4][0];
 		Erregea e1 = new Erregea(true);
 		taula[4][0].setPieza(e1);
-		PiezaZerrenda.getEMA().gehituPieza(e1);
 		
 		//Errege beltza
 		laukia = taula[4][7];
-		Erregea e2 = new Erregea(true);
+		Erregea e2 = new Erregea(false);
 		taula[4][7].setPieza(e2);
-		PiezaZerrenda.getEMA().gehituPieza(e2);
 		
 		//Erregin zuria
 		laukia = taula[3][0];
 		Erregina er1 = new Erregina(true);
 		taula[3][0].setPieza(er1);
-		PiezaZerrenda.getEMA().gehituPieza(er1);
 		
 		//Erregin beltza
 		laukia = taula[3][7];
 		Erregina er2 = new Erregina(false);
 		taula[3][7].setPieza(er2);
-		PiezaZerrenda.getEMA().gehituPieza(er2);
 	}
 
 	//Con este metodo vamos a mostrar el tablero después de cada jugada.
@@ -179,7 +161,7 @@ public class Taula {
 		pLaukia.setPieza(null);
 		//Laukia okupatuta badago aurreko metodoak kontuan hartuta soilik izan al dezake pieza etsaia
     		if (taula[pX][pY].getPieza() != null) {
-        		piezaJan(); 
+        		piezaJan(pX, pY); 
     		}
 		//Pieza bere posizio berrian jarri
     		taula[pX][pY].setPieza(p);
@@ -234,7 +216,7 @@ public class Taula {
 	}	
 	
 	public void peoiaHobetu(int pX, int pY){
-		Peoia pe = taula[pX][pY].getPieza();
+		Peoia pe = (Peoia) taula[pX][pY].getPieza();
 		boolean zuriaDa = pe.getZuriaDa();
 
 		//Konprobatzen dugu ea peoia ailegatu den azken lerrora.
