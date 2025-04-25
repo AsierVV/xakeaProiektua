ef81ebee0ba96a394ed9dbb23c72c312026fde07
AsierVV <avaldelviravazquez@gmail.com>
2025-04-25 15:52:31 +0200

    Update Jokoa.java
    Jokoa actualizado, MUY COMPLETO

--- xakeaProiektua/src/org/pmobo/xakea/Jokoa.java
+++ xakeaProiektua/src/org/pmobo/xakea/Jokoa.java
@@ -2,6 +2,7 @@ package org.pmobo.xakea;
 
 public class Jokoa {
 
+        Teklatua t = Teklatua.getTeklatua(); // Hartzen dugu Teklatuaren instantzia t aldagaia Teklatua bezala erabiltzeko.
         public void partidaBatJolastu() {
             taula = new Taula();                   // Taula sortu
             taula.hasieratuTaulaPiezak();         // Piezak sortu eta kokatu
@@ -17,23 +18,23 @@ public class Jokoa {
                     System.out.println("Beltzaren txanda:");
                 }
         
-                // Mugimendua irakurri TEKLATUA-rekin
-                int x1 = Teklatua.getTeklatua().irakurri("Aukeratu PIEZAren X posizioa:");
-                int y1 = Teklatua.getTeklatua().irakurri("Aukeratu PIEZAren Y posizioa:");
+                // Mugimendua irakurriIntInt TEKLATUA-rekin
+                int x1 = Teklatua.getTeklatua().irakurriInt("Aukeratu PIEZAren X posizioa:");
+                int y1 = Teklatua.getTeklatua().irakurriInt("Aukeratu PIEZAren Y posizioa:");
                 Laukia laukiaHasiera = taula.getLaukia(x1, y1);
                 Pieza pieza = laukiaHasiera.getPieza();
 
                 // While honetan konprobatuko da aukeratutako pieza existitzen den ala ez eta egokia den ala ez.
                 while (pieza == null || (pieza.getZuriaDa() && !bandaZuri) || (!pieza.getZuriaDa() && bandaZuri) {
                         System.out.println("Ez dago piezarik edo aukeratutako pieza ez da zure kolorekoa, aukeratu beste lauki berri bat.");
-                        int x1 = Teklatua.getTeklatua().irakurri("Aukeratu PIEZAren X posizioa:");
-                        int y1 = Teklatua.getTeklatua().irakurri("Aukeratu PIEZAren Y posizioa:");
+                        int x1 = Teklatua.getTeklatua().irakurriInt("Aukeratu PIEZAren X posizioa:");
+                        int y1 = Teklatua.getTeklatua().irakurriInt("Aukeratu PIEZAren Y posizioa:");
                         Laukia laukiaHasiera = taula.getLaukia(x1, y1);
                         Pieza pieza = laukiaHasiera.getPieza();
                 }
                     
-                int x2 = Teklatua.getTeklatua().irakurri("Aukeratu MUGIMENDUren X posizioa:");
-                int y2 = Teklatua.getTeklatua().irakurri("Aukeratu MUGIMENDUren Y posizioa:");
+                int x2 = Teklatua.getTeklatua().irakurriInt("Aukeratu MUGIMENDUren X posizioa:");
+                int y2 = Teklatua.getTeklatua().irakurriInt("Aukeratu MUGIMENDUren Y posizioa:");
                 Laukia laukiaHelburu = taula.getLaukia(x2, y2);
         
                 boolean lortuMugimenduEgokia = false;   // mugimendua noiz den egokia jakiteko
@@ -42,19 +43,19 @@ public class Jokoa {
                     if (!pieza.mugimenduEgokia(x2, y2, laukiaHelburu)) {
                         System.out.println("Mugimendua ez da egokia, sartu beste posizio bat pieza mugitzeko.");
                         // Eskatzen da berriro laukiaHelburu korrdenatuak
-                        int x2 = Teklatua.getTeklatua().irakurri("Aukeratu MUGIMENDUren X posizioa:");
-                        int y2 = Teklatua.getTeklatua().irakurri("Aukeratu MUGIMENDUren Y posizioa:");
+                        int x2 = Teklatua.getTeklatua().irakurriInt("Aukeratu MUGIMENDUren X posizioa:");
+                        int y2 = Teklatua.getTeklatua().irakurriInt("Aukeratu MUGIMENDUren Y posizioa:");
                         Laukia laukiaHelburu = taula.getLaukia(x2, y2);
                     } else {
                         lortuMugimenduEgokia = true;
                     }
                 }
         
-                // Mugimendua egitea "METODO SIN IMPLEMENTAR (EN TAULA)"
+                // Mugimendua egitea
                 taula.mugitu(laukiaHasiera, x2, y2); // El metodo "mugitu" movera la pieza a su nueva posicion y pondra un mensaje de que la pieza se ha movido.
                                                                 // Este metodo tb se tiene que encargar de llamar al metodo piezaJan, asi que comprobara si en las coordenadas x2 | y2 hay una pieza del color
                                                                  // contrario, y en ese caso, llamara al metodo piezaJan para que elimine esa pieza.
-               //Peoi bat mugi bada, hobetu deitu
+               //Peoi bat mugi bada, peoiaHobetu deitu
                 if (laukiaHasiera.getPieza() instanceof Peoia){
                         peoiaHobetu(x2, y2);
                 }
@@ -66,7 +67,6 @@ public class Jokoa {
                 partidaAmaituta = partidaAmaituta();
             }
         
-            System.out.println("Partida amaitu da!");
         }
 
         public void aldatuTxanda(){
