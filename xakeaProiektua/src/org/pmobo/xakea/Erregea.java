4965ad12b325fce79b6853c79c14ef6f1fe6a07d
marvoloelmago <marcomateosgallego@gmail.com>
2025-04-25 13:45:10 +0200

    Update Erregea.java

--- xakeaProiektua/src/org/pmobo/xakea/Erregea.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregea.java
@@ -3,8 +3,8 @@ package org.pmobo.xakea;
 public class Erregea extends Pieza{
 
 	//eraikitzailea
-	public Erregea(boolean pZuriaDa, Laukia pLaukia) {
-		super(pZuriaDa, pLaukia);
+	public Erregea(boolean pZuriaDa) {
+		super(pZuriaDa);
 	}
 
 	@Override
