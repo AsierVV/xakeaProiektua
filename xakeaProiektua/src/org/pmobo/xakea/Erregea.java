890ef4d20245a4f618220a33ac4f9ecbb0a03e03
anderRaya <anderraya10@gmail.com>
2025-04-25 13:15:49 +0200

    Update Erregea.java
    He añadido un ¨If¨ para que la pieza no pueda salirse del tablero

--- xakeaProiektua/src/org/pmobo/xakea/Erregea.java
+++ xakeaProiektua/src/org/pmobo/xakea/Erregea.java
@@ -9,6 +9,9 @@ public class Erregea extends Pieza{
 
 	@Override
 	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia){
+		if (pX < 0 || pX > 7 || pY < 0 || pY > 7) {
+	        return false;
+		}
 		if((pX==pLaukia.getX() && pY-pLaukia.getY()==1) || (pX-pLaukia.getX()==1 && pY-pLaukia.getY()==1) || (pX-pLaukia.getX()==1 && pY==pLaukia.getY()) || (pX-pLaukia.getX()==1 && pY-pLaukia.getY()==-1) || (pX==pLaukia.getX() && pY-pLaukia.getY()==-1) || (pX-pLaukia.getX()==-1 && pY-pLaukia.getY()==-1) || (pX-pLaukia.getX()==-1 && pY==pLaukia.getY()) || (pX-pLaukia.getX()==-1 && pY-pLaukia.getY()==1)){
 			//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
 			//              --> true: okupatuta dago bere kolore berdina duen piezaz
