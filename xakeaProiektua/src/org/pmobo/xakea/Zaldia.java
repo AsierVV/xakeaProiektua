576c7a44c8813177a9e42bec8f9a8a2c2660c1c9
AsierVV <avaldelviravazquez@gmail.com>
2025-04-25 22:21:14 +0200

    Update Zaldia.java
    Corregido teniendo en cuenta los errores de eclipse

--- xakeaProiektua/src/org/pmobo/xakea/Zaldia.java
+++ xakeaProiektua/src/org/pmobo/xakea/Zaldia.java
@@ -14,7 +14,7 @@ public class Zaldia extends Pieza {
 		if((pY-pLaukia.getY()==2 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)) || (pY-pLaukia.getY()==-2 && (pX-pLaukia.getX()==1 || pX-pLaukia.getX()==-1)) || (pX-pLaukia.getX()==2 && (pY-pLaukia.getY()==1 || pY-pLaukia.getY()==-1)) || (pX-pLaukia.getX()==-2 &&(pY-pLaukia.getY()==1 || pY-pLaukia.getY()==-1))) {
 			//okupatutaDago --> false: ez dago okupatuta edo okupatuta dago bere kolore ezberdina duen piezaz
 			//              --> true: okupatuta dago bere kolore berdina duen piezaz
-			if (okupatutaDago(pX, pY, pZuriaDa)){
+			if(Taula.getTaula().okupatutaDago(pX, pY, this.getZuriaDa())){
 				return false;
 			} else{return true;}
 		} else{return false;}
