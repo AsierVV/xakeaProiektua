9bf0f780dbaff6f3596a89ffb4dd20f659f4187d
marvoloelmago <marcomateosgallego@gmail.com>
2025-04-25 13:45:39 +0200

    Update Alfila.java

--- xakeaProiektua/src/org/pmobo/xakea/Alfila.java
+++ xakeaProiektua/src/org/pmobo/xakea/Alfila.java
@@ -3,8 +3,8 @@ package org.pmobo.xakea;
 public class Alfila extends Pieza {
 
 	//eraikitzailea
-	public Alfila(boolean pZuriaDa, Laukia pLaukia) {
-		super(pZuriaDa, pLaukia);
+	public Alfila(boolean pZuriaDa) {
+		super(pZuriaDa);
 	}
 	@Override
 	public boolean mugimenduEgokia(int pX, int pY, Laukia pLaukia) {
